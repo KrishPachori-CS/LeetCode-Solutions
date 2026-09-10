63ceca3350f73def34bb9e8a37549a69a9ea1e6f
Krish Pachori <krishpachori18@gmail.com>
2026-09-10 08:38:17 +0530

    Time: 3 ms (70.26%), Space: 53.6 MB (67.68%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int t = trust.length;
+        if (t == 0 && n == 1) 
+            return 1;
+        int[] count = new int[n + 1];
+        for (int[] i : trust) {
+            count[i[0]]--;
+            count[i[1]]++;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] == n - 1) return i;
+        }
+        return -1;
+    }
+}
